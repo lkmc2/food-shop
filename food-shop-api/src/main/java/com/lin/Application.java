e67cb90d1277dfc4175f0ab79c3a7927a55ff561
lkmc2 <lkmc2@163.com>
2020-03-07 21:04:12 +0800

    添加注解扫描通用Mapper所在位置

--- food-shop-api/src/main/java/com/lin/Application.java
+++ food-shop-api/src/main/java/com/lin/Application.java
@@ -2,6 +2,7 @@ package com.lin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * Spring Boot 启动类
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2020/3/7 14:02
  */
 @SpringBootApplication
+@MapperScan(basePackages = "com.lin.dao")
 public class Application {
 
     public static void main(String[] args) {
