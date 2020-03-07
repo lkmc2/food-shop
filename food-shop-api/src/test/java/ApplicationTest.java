238bf820c3890c47c9ec4eca2e2f852720662efb
lkmc2 <lkmc2@163.com>
2020-03-07 21:11:27 +0800

    添加测试依赖

---
+++ food-shop-api/src/test/java/ApplicationTest.java
@@ -0,0 +1,14 @@
+import com.lin.Application;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 应用程序测试
+ * @author lkmc2
+ * @date 2020/3/7 21:10
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class ApplicationTest {
+}
