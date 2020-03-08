86b27da1a87d807f8b867a77f8eb62b03c26c1ae
lkmc2 <lkmc2@163.com>
2020-03-08 17:30:32 +0800

    添加设置session的例子

--- food-shop-api/src/main/java/com/lin/controller/HelloController.java
+++ food-shop-api/src/main/java/com/lin/controller/HelloController.java
@@ -1,8 +1,13 @@
 package com.lin.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * 示范 Controller
@@ -13,9 +18,29 @@ import springfox.documentation.annotations.ApiIgnore;
 @RestController
 public class HelloController {
 
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @GetMapping("/hello")
     public String hello() {
         return "Hello World";
+    }
+
+    @GetMapping("/setSession")
+    public Object setSession(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        // 设置 session 信息
+        session.setAttribute("userInfo", "new user");
+        // 设置过期时长
+        session.setMaxInactiveInterval(3600);
+
+        // 获取 session 信息
+        Object userInfo = session.getAttribute("userInfo");
+        logger.info("userInfo = 【{}】", userInfo);
+
+        // 移除 session 信息
+        session.removeAttribute("userInfo");
+
+        return "ok";
     }
 
 }
