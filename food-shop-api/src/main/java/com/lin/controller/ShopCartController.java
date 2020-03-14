ce613eebe2ab93f8f6d94d5c2a562925e65262c2
lkmc2 <lkmc2@163.com>
2020-03-14 16:57:10 +0800

    添加从购物车中删除商品的接口

--- food-shop-api/src/main/java/com/lin/controller/ShopCartController.java
+++ food-shop-api/src/main/java/com/lin/controller/ShopCartController.java
@@ -5,6 +5,7 @@ import com.lin.bo.ShopCartBO;
 import com.lin.utils.JsonResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
@@ -37,6 +38,27 @@ public class ShopCartController extends BaseController {
         System.out.println(shopCartBO);
 
         // todo：前端用户在登陆的情况下，添加商品到购物车，会同时在后端同步购物车到redis数据库
+
+        return JsonResult.ok();
+    }
+
+    @ApiOperation(value = "从购物车中删除商品", notes = "从购物车中删除商品")
+    @ApiImplicitParams({
+        @ApiImplicitParam(name = "userId", value = "用户id", required = true),
+        @ApiImplicitParam(name = "itemSpecId", value = "商品规格id", required = true)
+    })
+    @PostMapping("/del")
+    public JsonResult del(
+            @RequestParam String userId,
+            @RequestParam String itemSpecId,
+            HttpServletRequest request,
+            HttpServletResponse response) {
+
+        if (StrUtil.isBlank(userId) || StrUtil.isBlank(itemSpecId)) {
+            return JsonResult.errorMsg("参数不能为空");
+        }
+
+        // todo：用户在页面删除购物车中的商品数据，如果此时用户已经登陆，则需要同步删除后端购物车中的数据
 
         return JsonResult.ok();
     }
