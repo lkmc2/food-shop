23fe73d33ce328656d8e1ed71a2f01627c34f63f
lkmc2 <lkmc2@163.com>
2020-03-15 15:49:21 +0800

    添加通知商户支付成功的url

--- food-shop-api/src/main/java/com/lin/controller/BaseController.java
+++ food-shop-api/src/main/java/com/lin/controller/BaseController.java
@@ -16,4 +16,7 @@ public class BaseController {
     /** 每页评论显示条数 **/
     protected static final int COMMENT_PAGE_SIZE = 10;
 
+    /** 通知商户支付成功的 url ，微信支付成功 -> 支付中心 -> food-shop **/
+    protected static final String PAY_RETURN_URL = "http://localhost:8088/orders/notifyMerchantOrderPaid";
+
 }
