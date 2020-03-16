eeafa5eaa548c364046a0c899c4e60fd2b71b812
lkmc2 <lkmc2@163.com>
2020-03-16 20:34:30 +0800

    添加静态资源的映射

--- food-shop-api/src/main/java/com/lin/config/WebMvcConfig.java
+++ food-shop-api/src/main/java/com/lin/config/WebMvcConfig.java
@@ -4,6 +4,8 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Web MVC 配置
@@ -11,7 +13,7 @@ import org.springframework.web.client.RestTemplate;
  * @date 2020/3/15 16:24
  */
 @Configuration
-public class WebMvcConfig {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     /**
      * 创建 RestTemplate 对象
@@ -21,6 +23,16 @@ public class WebMvcConfig {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
+    }
+
+    /**
+     * 实现静态资源的映射
+     * @param registry 资源注册器
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("file:E:/UploadWorkplace/FoodShowPicUpload/");
     }
 
 }
