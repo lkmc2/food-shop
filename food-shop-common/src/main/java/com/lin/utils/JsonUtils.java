856ac5fa1d3d2209bc3aaea2a832156eb6e44cde
lkmc2 <lkmc2@163.com>
2020-04-05 21:05:56 +0800

    修改工具类的注释

--- food-shop-common/src/main/java/com/lin/utils/JsonUtils.java
+++ food-shop-common/src/main/java/com/lin/utils/JsonUtils.java
@@ -1,66 +1,62 @@
 package com.lin.utils;
 
+import cn.hutool.core.util.StrUtil;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.collect.Lists;
 
 import java.util.List;
 
 /**
- * 
  * Json 工具类
+ * @author lkmc2
+ * @date 2020/4/5 20:58
  */
 public class JsonUtils {
 
-    /** 定义 jackson 对象 **/
+    /** jackson 转换器 **/
     private static final ObjectMapper MAPPER = new ObjectMapper();
 
     /**
-     * 将对象转换成json字符串。
-     * @param data
-     * @return
+     * 将对象转换成 json 字符串
+     * @param data 实体类对象
+     * @return json 字符串
      */
     public static String objectToJson(Object data) {
     	try {
             return MAPPER.writeValueAsString(data);
 		} catch (JsonProcessingException e) {
-			e.printStackTrace();
+            throw new RuntimeException(StrUtil.format("实体类【{}】转换成json字符串失败", data.getClass().getName()), e);
 		}
-    	return null;
     }
     
     /**
-     * 将json结果集转化为对象
-     * 
-     * @param jsonData json数据
-     * @param beanType 对象中的object类型
-     * @return
+     * 将 json 字符串转化为实体类对象
+     * @param jsonStr json 字符串
+     * @param beanType 目标实体类类型
+     * @return 实体类对象
      */
-    public static <T> T jsonToPojo(String jsonData, Class<T> beanType) {
+    public static <T> T jsonToPojo(String jsonStr, Class<T> beanType) {
         try {
-            return MAPPER.readValue(jsonData, beanType);
+            return MAPPER.readValue(jsonStr, beanType);
         } catch (Exception e) {
-        	e.printStackTrace();
+            throw new RuntimeException(StrUtil.format("json字符串转换成实体类【{}】失败", beanType.getName()), e);
         }
-        return null;
     }
     
     /**
      * 将json数据转换成pojo对象list
-     * @param jsonData
-     * @param beanType
-     * @return
+     * @param jsonStr json 字符串
+     * @param beanType 目标实体类类型
+     * @return 实体类列表
      */
-    public static <T> List<T> jsonToList(String jsonData, Class<T> beanType) {
+    public static <T> List<T> jsonToList(String jsonStr, Class<T> beanType) {
     	JavaType javaType = MAPPER.getTypeFactory().constructParametricType(List.class, beanType);
     	try {
-            return MAPPER.readValue(jsonData, javaType);
+            return MAPPER.readValue(jsonStr, javaType);
 		} catch (Exception e) {
-			e.printStackTrace();
+            throw new RuntimeException(StrUtil.format("json字符串转换成实体类【{}】列表失败", beanType.getName()), e);
 		}
-    	
-    	return Lists.newArrayList();
     }
     
 }
