f834dccf81e31edfb7c045a9304c07833ff4fb1d
lkmc2 <lkmc2@163.com>
2020-04-05 11:51:41 +0800

    为首页轮播图设置redis缓存

--- food-shop-api/src/main/java/com/lin/controller/IndexController.java
+++ food-shop-api/src/main/java/com/lin/controller/IndexController.java
@@ -1,11 +1,14 @@
 package com.lin.controller;
 
+import cn.hutool.core.util.StrUtil;
 import com.lin.enums.YesOrNoEnum;
 import com.lin.pojo.Carousel;
 import com.lin.pojo.Category;
 import com.lin.service.CarouselService;
 import com.lin.service.CategoryService;
 import com.lin.utils.JsonResult;
+import com.lin.utils.JsonUtils;
+import com.lin.utils.RedisOperator;
 import com.lin.vo.CategoryVO;
 import com.lin.vo.NewItemsVO;
 import io.swagger.annotations.Api;
@@ -35,12 +38,34 @@ public class IndexController {
     @Autowired
     private CategoryService categoryService;
 
+    @Autowired
+    private RedisOperator redisOperator;
+
     @ApiOperation(value = "获取首页轮播图列表", notes = "获取首页轮播图列表")
     @GetMapping("/carousel")
     public JsonResult carousel() {
-        List<Carousel> list = carouselService.queryAll(YesOrNoEnum.YES.type);
+        // 查询缓存
+        String carouselStr = redisOperator.get("carousel");
+
+        List<Carousel> list;
+
+        if (StrUtil.isBlank(carouselStr)) {
+            // 从数据库中查询，并设置到缓存中
+            list = carouselService.queryAll(YesOrNoEnum.YES.type);
+            redisOperator.set("carousel", JsonUtils.objectToJson(list));
+        } else {
+            // 直接加载缓存
+            list = JsonUtils.jsonToList(carouselStr, Carousel.class);
+        }
+
         return JsonResult.ok(list);
     }
+
+    /**
+     1.后台运行系统，一旦广告（轮播图）发生更改，就可以删除缓存，然后重置
+     2.定时重置，比如每天凌晨三点重置
+     3.每个轮播图都有可能是一个广告，每个广告都会有一个过期时间，过期了，再重置
+     */
 
     /**
      * 首页分类展示需求
@@ -50,7 +75,20 @@ public class IndexController {
     @ApiOperation(value = "获取商品分类（一级分类）", notes = "获取商品分类（一级分类）")
     @GetMapping("/cats")
     public JsonResult cats() {
-        List<Category> list = categoryService.queryAllRootLevelCat();
+        // 查询缓存
+        String catsStr = redisOperator.get("cats");
+
+        List<Category> list;
+
+        if (StrUtil.isBlank(catsStr)) {
+            // 从数据库中查询，并设置到缓存中
+            list = categoryService.queryAllRootLevelCat();
+            redisOperator.set("cats", JsonUtils.objectToJson(list));
+        } else {
+            // 直接加载缓存
+            list = JsonUtils.jsonToList(catsStr, Category.class);
+        }
+
         return JsonResult.ok(list);
     }
 
@@ -76,7 +114,20 @@ public class IndexController {
             return JsonResult.errorMsg("分类不存在");
         }
 
-        List<NewItemsVO> list = categoryService.getSixNewItemLazy(rootCatId);
+        // 查询缓存
+        String sixNewItemsStr = redisOperator.get(StrUtil.format("sixNewItems/{}", rootCatId));
+
+        List<NewItemsVO> list;
+
+        if (StrUtil.isBlank(sixNewItemsStr)) {
+            // 从数据库中查询，并设置到缓存中
+            list = categoryService.getSixNewItemLazy(rootCatId);
+            redisOperator.set(StrUtil.format("sixNewItems/{}", rootCatId), JsonUtils.objectToJson(list));
+        } else {
+            // 直接加载缓存
+            list = JsonUtils.jsonToList(sixNewItemsStr, NewItemsVO.class);
+        }
+
         return JsonResult.ok(list);
     }
 
